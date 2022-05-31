b2e31833717b2a2bbabe0ea3be0bd22b26faa97a
Tomas Pesek <pesekt@gmail.com>
2022-05-31 15:54:17 +0200

    parsing request bodies

--- routers/reminders.ts
+++ routers/reminders.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import CreateReminderDto from "../dtos/CreateReminderDto";
 
 const router = Router(); //returns a router
 router.get("/", (req, res) => {
   res.send("lsit of reminders");
+});
+
+router.post("/", (req, res) => {
+  const { title } = req.body as CreateReminderDto;
+  res.json(title);
 });
 
 export default router; //export the router object
