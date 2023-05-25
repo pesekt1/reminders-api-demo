639b931eb6e2d3195a222f8db967072c33305f86
Tomas Pesek <TOMP@kea.dk>
2023-05-25 23:02:15 +0200

    add cors

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import remindersRouter from "./routers/reminders";
+import cors from "cors";
 
 const app = express();
+app.use(cors()); //use cors to allow cross-origin requests
 
 app.use(express.json()); //for express so that it parses incoming request bodies.
-app.use("/reminders", remindersRouter);
+app.use("/todos", remindersRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
